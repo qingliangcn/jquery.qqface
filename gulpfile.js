7b877d91b27895ec5840d10d4fc07ba66b5514b1
kyo4311 <cff20@sina.com>
2016-03-08 21:02:05 +0800

    js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require("gulp");
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+
+
 
 gulp.task('lint', function() {
     return gulp.src('./jquery.qqface.js')
