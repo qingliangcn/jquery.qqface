9d3730e5c13ebb3d8a6b91baac90e0a3b4fdd390
kyo4311 <cff20@sina.com>
2016-03-08 21:19:14 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require("gulp");
-var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 
-
-
-gulp.task('lint', function() {
-    return gulp.src('./jquery.qqface.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter());
-});
+// gulp.task('lint', function() {
+//     return gulp.src('./jquery.qqface.js')
+//         .pipe(jshint())
+//         .pipe(jshint.reporter());
+// });
 
 gulp.task('js', function() {
     return gulp.src('jquery.qqface.js')
