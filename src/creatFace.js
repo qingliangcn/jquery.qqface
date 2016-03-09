d21ab5e0dcc964af1e87f32f54dabe09fa9a5ea5
kyo4311 <cff20@sina.com>
2016-03-09 11:50:20 +0800

    textarea

--- src/creatFace.js
+++ src/creatFace.js
@@ -3,6 +3,6 @@ module.exports = function(arr, imgPath) {
     return $.map(arr, function(item, index) {
         item = item.split(',');
         var src = imgPath + item[0] + '.gif';
-        return '<i data-code="' + item[1] + '"><img src="' + src + '" width="24" height="24" title="' + item[1] + '" /></i>'
+        return '<i data-code="' + item[1] + '"><img src="' + src + '" width="24" height="24" title="' + item[1] + '" /></i>';
     }).join('');
 }
